5f69cbb9cfecc93d3b782437a172294f23023270
dgash485 <59995121+dgash485@users.noreply.github.com>
2021-03-10 17:57:30 +0300

    Add files via upload

---
+++ Console App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.FSharp.Collections;
+using QuickSort;
+
+namespace Console_App
+{
+    class Program
+    {
+        static void PrintEnumerable<T>(IEnumerable<T> list)
+        {
+            foreach (T element in list)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var original = new List<int>();
+            var random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                original.Add(random.Next() % 25);
+            }
+            PrintEnumerable<int>(original);
+
+            var toSort = ListModule.OfSeq(original);
+            var sorted = QSort.sort<int>(toSort);
+
+            PrintEnumerable<int>(sorted);
+        }
+    }
+}
