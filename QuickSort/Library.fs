58f7b55b37fc96298a412f1f92c427c54e7658d9
dgash485 <59995121+dgash485@users.noreply.github.com>
2021-03-10 17:51:28 +0300

    Add files via upload

---
+++ QuickSort/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace QuickSort
+
+module QSort =
+    let rec sort list =
+        match list with
+        | [] -> []
+        | x :: list ->
+            let smaller = sort (list |> List.filter(fun e -> e <= x))
+            let bigger  = sort (list |> List.filter(fun e -> e >  x))
+            smaller @ [x] @ bigger
